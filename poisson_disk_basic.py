aff17b7235b8d469aab2e8e302356871573243b0
chunleili <li_cl@foxmail.com>
2022-07-26 22:46:22 +0800

    update hard

--- poisson_disk_basic.py
+++ poisson_disk_basic.py
@@ -2,9 +2,8 @@ import taichi as ti
 import taichi.math as tm
 ti.init(arch=ti.cpu)
 
-grid_nx = 400
-grid_ny = 800
-res = (grid_nx, grid_ny)
+grid_n = 400
+res = (grid_n, grid_n)
 dx = 1 / res[0]
 inv_dx = res[0]
 radius = dx * ti.sqrt(2)
@@ -18,8 +17,8 @@ grid.fill(-1)
 def check_collision(p, index):
     x, y = index
     collision = False
-    for i in range(max(0, x - 2), min(grid_nx, x + 3)):
-        for j in range(max(0, y - 2), min(grid_ny, y + 3)):
+    for i in range(max(0, x - 2), min(grid_n, x + 3)):
+        for j in range(max(0, y - 2), min(grid_n, y + 3)):
             if grid[i, j] != -1:
                 q = samples[grid[i, j]]
                 if (q - p).norm() < radius - 1e-6:
@@ -28,8 +27,8 @@ def check_collision(p, index):
 
 @ti.kernel
 def poisson_disk_sample(desired_samples: int) -> int:
-    samples[0] = tm.vec2(0.5/2, 0.5)
-    grid[int(grid_nx * 0.5), int(grid_ny * 0.5)] = 0
+    samples[0] = tm.vec2(0.5)
+    grid[int(grid_n * 0.5), int(grid_n * 0.5)] = 0
     head, tail = 0, 1
     while head < tail and head < desired_samples:
         source_x = samples[head]
@@ -41,7 +40,7 @@ def poisson_disk_sample(desired_samples: int) -> int:
             new_x = source_x + offset
             new_index = int(new_x * inv_dx)
 
-            if 0 <= new_x[0] < (res[0]/res[1]) and 0 <= new_x[1] < 1:
+            if 0 <= new_x[0] < 1 and 0 <= new_x[1] < 1:
                 collision = check_collision(new_x, new_index)
                 if not collision and tail < desired_samples:
                     samples[tail] = new_x
